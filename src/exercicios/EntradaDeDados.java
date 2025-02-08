54a6dc96d1865033c2f43c7319fb1ec4468fddb0
Renaisa <renaisaalves@gmail.com>
2025-02-08 04:55:05 -0300

    Salvando mais uma Classe para estudos de Java

---
+++ src/exercicios/EntradaDeDados.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class EntradaDeDados {
+	
+	public static void main(String[] args) {
+		
+		String name = "Renaisa";
+		System.out.printf("Boa menina, %s", name);
+	}
+
+}
