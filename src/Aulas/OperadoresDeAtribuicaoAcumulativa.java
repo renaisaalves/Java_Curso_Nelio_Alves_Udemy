3ae66410801f59f6ce9e225684da33716d1497ab
Renaisa <renaisaalves@gmail.com>
2025-02-10 07:00:50 -0300

    atualização

---
+++ src/Aulas/OperadoresDeAtribuicaoAcumulativa.java
@@ -0,0 +1,34 @@
+package Aulas;
+
+import java.util.Scanner;
+
+public class OperadoresDeAtribuicaoAcumulativa {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int minutos = scan.nextInt();
+		
+		double conta = 50.00;
+		if  (minutos  > 100) {
+			conta = conta + (minutos - 100) * 2.0; // conta = conta + pode ser substituído por conta += conta
+			
+		}
+		
+		System.out.printf("Valor da conta: R$%.2f%n", conta);
+		
+		scan.close();
+
+	}
+
+}
+
+//OPERADORES DE ATRIBUIÇÃO ACUMULATIVA
+
+/* É quando a variável vai somar, dividir, multiplicar com outra variável e armazenar nela mesma
+ * a += b ou a = a + b
+ * a -= b
+ * a *= b
+ * a /= b
+ * a %= b */
--- src/exerciciosCondicionaisLogicos/Exercicio9CLAULA.java
+++ src/exerciciosCondicionaisLogicos/Exercicio9CLAULA.java
@@ -10,15 +10,20 @@ public class Exercicio9CLAULA {
 		
 		System.out.println("PLANO BÁSICO TELEFONE\nR$50,00 por 100 minutos!\nAo exceder esse limite, será cobrado uma taxa de R$2,00 por minuto consumido");
 		
-		System.out.print("Consumo:");
+		System.out.print("Consumo: ");
 		int minutosFalados = scan.nextInt();
 		
-		if (minutosFalados == 100) {
+		if (minutosFalados <= 100) {
 			System.out.printf("Preço: R$50,00%n");
 		} else {
+			double preco = ((minutosFalados - 100) * 2) + 50;
+			double precoExcesso = preco - 50;
+			System.out.printf("Preço: R$%.2f%nPreço por excesso R$%.2f%n", preco, precoExcesso);
 			
 		}
 		scan.close();
+		
+		//EM CONSTRUÇÃO
 
 	}
 
