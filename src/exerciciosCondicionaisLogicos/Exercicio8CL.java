d21d4bb37af601ded1a6cabf6b8b3deafff0c25b
Renaisa <renaisaalves@gmail.com>
2025-02-10 06:12:56 -0300

    atualizando exercício da aula

--- src/exerciciosCondicionaisLogicos/Exercicio8CL.java
+++ src/exerciciosCondicionaisLogicos/Exercicio8CL.java
@@ -1,10 +1,32 @@
 package exerciciosCondicionaisLogicos;
 
+import java.util.Scanner;
+
 public class Exercicio8CL {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		
 		// 8) Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$. Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+		
+		System.out.print("Salário: ");
+		double salario = scan.nextDouble();
+		
+		if (salario > 0.00 && salario <= 2000.00) {
+			System.out.println("ISENTO");
+		} else if (salario > 2000.01 && salario < 3000.00) {
+			double imposto = (8 * salario) / 100;
+			System.out.printf("Imposto: R$ %.2f%n", imposto);
+		} else if (salario > 3000.01 && salario <= 4500.00) {
+			double imposto = (18 * salario) / 100;
+			System.out.printf("Imposto: R$ %.2f%n", imposto);
+		} else {
+			double imposto = (28 * salario) / 100;
+			System.out.printf("Imposto: R$ %.2f%n", imposto);
+		}
+		
+		scan.close();
 
 	}
 
---
+++ src/exerciciosCondicionaisLogicos/Exercicio9CLAULA.java
@@ -0,0 +1,25 @@
+package exerciciosCondicionaisLogicos;
+
+import java.util.Scanner;
+
+public class Exercicio9CLAULA {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("PLANO BÁSICO TELEFONE\nR$50,00 por 100 minutos!\nAo exceder esse limite, será cobrado uma taxa de R$2,00 por minuto consumido");
+		
+		System.out.print("Consumo:");
+		int minutosFalados = scan.nextInt();
+		
+		if (minutosFalados == 100) {
+			System.out.printf("Preço: R$50,00%n");
+		} else {
+			
+		}
+		scan.close();
+
+	}
+
+}
