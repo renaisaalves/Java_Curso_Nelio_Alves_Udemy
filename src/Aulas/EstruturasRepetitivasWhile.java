6eaab20e50012e89553f17fb65524fc77e7cf9e2
Renaisa <renaisaalves@gmail.com>
2025-02-11 19:55:06 -0300

    atualizando

--- src/Aulas/EstruturasRepetitivasWhile.java
+++ src/Aulas/EstruturasRepetitivasWhile.java
@@ -1,11 +1,24 @@
 package Aulas;
 
+import java.util.Scanner;
+
 public class EstruturasRepetitivasWhile {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
 		
-
+		int num = scan.nextInt();
+		int soma = 0;
+		
+		while (num != 0){
+			soma = soma + num;
+			num = scan.nextInt();
+		}
+		
+		System.out.println("Soma: " + soma);
+		
+		scan.close();
 	}
 
 }
