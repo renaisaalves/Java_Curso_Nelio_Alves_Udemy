067bbc713d5c1296cc76ebc25852a20a816a3d05
Renaisa <renaisaalves@gmail.com>
2025-02-12 05:51:27 -0300

    atualização while

---
+++ src/exerciciosWhile/Exercicio1W.java
@@ -0,0 +1,10 @@
+package exerciciosWhile;
+
+public class Exercicio1W {
+
+	public static void main(String[] args) {
+		//1) Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+
+	}
+
+}
