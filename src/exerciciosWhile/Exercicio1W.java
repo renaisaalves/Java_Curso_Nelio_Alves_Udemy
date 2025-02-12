4def0dda064ed67ec5006da7a5df645938ac65d7
Renaisa <renaisaalves@gmail.com>
2025-02-12 15:15:04 -0300

    atualização

--- src/exerciciosWhile/Exercicio1W.java
+++ src/exerciciosWhile/Exercicio1W.java
@@ -1,9 +1,26 @@
 package exerciciosWhile;
 
+import java.util.Scanner;
+
 public class Exercicio1W {
 
 	public static void main(String[] args) {
 		//1) Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int senha = scan.nextInt();
+		
+		int senhaInserida = scan.nextInt();
+		
+		while (senhaInserida != senha) {
+			System.out.println("Senha inválida. Digite novamente: ");
+			senhaInserida = scan.nextInt();
+		}
+		
+		System.out.println("Acesso permitido");
+		
+		scan.close();
 
 	}
 
