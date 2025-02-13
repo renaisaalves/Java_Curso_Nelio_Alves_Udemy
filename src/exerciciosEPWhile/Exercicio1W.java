72f042cbcc2fde76912bfadc4920c9a06614f8e1
Renaisa <renaisaalves@gmail.com>
2025-02-13 04:56:10 -0300

    reorganizando diretórios

--- src/exerciciosEPWhile/Exercicio1W.java
+++ src/exerciciosEPWhile/Exercicio1W.java
@@ -1,4 +1,4 @@
-package exerciciosWhileForDoWhile;
+package exerciciosEPWhile;
 
 import java.util.Scanner;
 
--- src/exerciciosERFor/Exercicio2For.java
+++ src/exerciciosERFor/Exercicio2For.java
@@ -1,4 +1,4 @@
-package exerciciosWhileForDoWhile;
+package exerciciosERFor;
 
 import java.util.Scanner;
 
