d4d0f08a864dc97c3eb4f2f9dfaa7832cc61274e
Renaisa <renaisaalves@gmail.com>
2025-02-13 04:50:27 -0300

    atualização

---
+++ src/exerciciosWhileForDoWhile/Exercicio1For.java
@@ -0,0 +1,17 @@
+package exerciciosWhileForDoWhile;
+
+import java.util.Scanner;
+
+public class Exercicio1For {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		// 1) Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+		
+		scan.close();
+
+	}
+
+}
---
+++ src/exerciciosWhileForDoWhile/Exercicio2For.java
@@ -0,0 +1,21 @@
+package exerciciosWhileForDoWhile;
+
+import java.util.Scanner;
+
+public class Exercicio2For {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		// 2) Leia um valor inteiro N. Este valor será a quantidade de valores inteiros
+		// X que serão lidos em seguida. Mostre quantos destes valores X estão dentro do
+		// intervalo [10,20] e quantos estão fora do intervalo, mostrandoessas
+		// informações conforme exemplo (use a palavra "in" para dentro do intervalo, e
+		// "out" para fora do intervalo).
+
+		scan.close();
+
+	}
+
+}
