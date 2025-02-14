abc8f080243d810194f82d982cf636a81a37cb68
Renaisa <renaisaalves@gmail.com>
2025-02-14 06:58:08 -0300

    atualização

--- src/exerciciosERFor/Exercicio4For.java
+++ src/exerciciosERFor/Exercicio4For.java
@@ -15,7 +15,13 @@ public class Exercicio4For {
 		int n = scan.nextInt();
 
 		for (int i= 0; i<n; i++) {
-
+			double n1 = scan.nextDouble();
+			double n2 = scan.nextDouble();
+			double divisao = n1 / n2;
+			//if (divisao == 0) {
+			//	System.out.println("Divisão impossível");
+			//	}
+			System.out.println(divisao);
 		}
 		
 		
