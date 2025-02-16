18c205f8689daf2deb2047812c71b6479dc4d054
Renaisa <renaisaalves@gmail.com>
2025-02-16 07:36:49 -0300

    atualizando mais uma aula

---
+++ src/aulas/FormatacaoDeString.java
@@ -0,0 +1,43 @@
+package aulas;
+
+import java.util.Scanner;
+public class FormatacaoDeString {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		// FORMATAÇÃO DE STRING
+		
+		
+
+		
+		
+		
+		
+		scan.close();
+	}
+
+}
+
+/* FORMATAR
+ * Letra minúscula: toLowerCase()
+ * Letra maiúscula: toUpperCase()
+ * Não sei: trim()
+ * 
+ * RECORTAR
+ * substring(inicio)
+ * substring(inicio, fim)
+ * 
+ * SUBSTITUIR
+ * Replace(char, char)
+ * Replace(string, string)
+ * 
+ * BUSCAR
+ * IndexOf
+ * LastIndexOf
+ * 
+ * CORTAR
+ * str.split(" ")
+ * */
+ 
