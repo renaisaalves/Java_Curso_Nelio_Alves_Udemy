5a3651362bc5e374bee316277328c23817e273e8
Renaisa <renaisaalves@gmail.com>
2025-02-17 19:24:29 -0300

    atualizando

--- src/exerciciosAvulsos/AliquotaINSS.java
+++ src/exerciciosAvulsos/AliquotaINSS.java
@@ -15,11 +15,26 @@ public class AliquotaINSS {
 		
 		System.out.print("Qual categoria você pertence? [Individual ou Facultativo]\nResposta: ");
 		char categoriaEspecie = scan.next().charAt(0);
+		
 		while (categoriaEspecie != 'i' && categoriaEspecie != 'I' && categoriaEspecie != 'f' && categoriaEspecie != 'F') {
 			System.out.print("Categoria inválida. Por favor, insire a categoria novamente: ");
 			categoriaEspecie = scan.next().charAt(0);
-		}
+		} // Enquanto a resposta não for Individual ou Facultativo, o programa não prosseguirá para a próxima etapa.
 		
+		System.out.println("Qual alíquota você deseja recolher?\nResposta: ");
+		if (categoriaEspecie == 'i' && categoriaEspecie == 'I') {
+			int aliquota = scan.nextInt();
+			while(aliquota != 20 && aliquota != 11) {
+				System.out.print("Alíquota incorreta. Digite novamente: ");
+				aliquota = scan.nextInt();
+			}
+		} else {
+			int aliquota = scan.nextInt();
+			while(aliquota != 20 && aliquota != 11 && aliquota != 5) {
+				System.out.print("Alíquota incorreta. Digite novamente: ");
+				aliquota = scan.nextInt();
+			}
+		}
 		System.out.print("Está dando certo");
 		scan.close();
 
