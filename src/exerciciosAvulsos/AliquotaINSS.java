fc5c112f348494b1678e9eaa1a6df10984f3cfef
Renaisa <renaisaalves@gmail.com>
2025-02-17 19:04:12 -0300

    exercicio

--- src/exerciciosAvulsos/AliquotaINSS.java
+++ src/exerciciosAvulsos/AliquotaINSS.java
@@ -8,64 +8,19 @@ public class AliquotaINSS {
 
 		Scanner scan = new Scanner(System.in);
 
-		// CÁLCULO DA GUIA PREVIDÊNCIA SOCIAL (GPS) 2025
-		// Consiste em calcular o valor da parcela com base no código e porcentagem;
-		//  perguntar se trabalha como autônomo (individual) ou está desempregado
-		// (facultativo) 20%, 11%, 5%;
-		// Se for facultativo, oferecer a opção dos 5%
-		// Perguntar qual porcentagem de contribuição o usuário deseja aderir;
-		// Se o usuário escolher a porcentagem 11%, efetuar o cálculo com base no valor
-		// do salário mínimo vigente;
-		// Se o usuário que escolheu a modalidade facultativo escolher a opção dos 5%,
-		// perguntar se está inscrito no CAD-Único
-		// Se a pessoa estiver, fazer o cálculo; se não estiver, dizer que não é
-		// possível contribuir por essa porcentagem enquanto não tiver o cadastro no
-		// Cad-Único;
-		// se o usuário escolher as opções 20%, perguntar qual salário ele deseja
-		// contribuir;
-		// exemplo: se o usuário escolheu R$3000,00 calcular 20% de 3000,00
-		// exibir na tela o valor da taxa a ser paga.
+		System.out.println("CÁLCULO DA GUIA DA PREVIDÊNCIA SOCIAL (GPS) 2025\n");
 		
-		double salarioMin = 1518.00;
-		double valor = 0.0;
-		System.out.println("CÁLCULO DA GUIA PREVIDÊNCIA SOCIAL (GPS)");
-		System.out.print("Você trabalha como autônomo? (por conta própria)\nR: ");
-		char resp = scan.next().charAt(0);
+		System.out.println("Contribuinte Individual:\nCódigo 1007 [20%]\nCódigo 1163 [11%]\n");
+		System.out.println("Contribuinte Facultativo:\nCódigo 1406 [20%]\nCódigo 1473 [11%]\nCódigo 1929 [5%]\n");
 		
-		
-		
-		
-		if (resp == 's' || resp == 'S') {
-			System.out.println("Então você se enquadra na categoria Contribuinte Individual\n");
-			System.out.print("Qual alíquota deseja recolher?\n[20%] [11%]: ");
-			int aliquota = scan.nextInt();
-			while (aliquota != 20 && aliquota != 11) {
-					System.out.print("Valor inválido\nDigite novamente: ");
-					aliquota = scan.nextInt();
-				}
-		} else if (resp == 'n' || resp == 'N'){
-			System.out.println("Então você se enquadra na categoria Contribuinte Facultativo\n");
-			System.out.print("Qual alíquota deseja recolher?\n[20%] [11%] [5%]: ");
-			int aliquota = scan.nextInt();
-			while (aliquota != 20 && aliquota != 11 & aliquota != 5) {
-					System.out.print("Valor inválido\nDigite novamente: ");
-					aliquota = scan.nextInt();
-				}
+		System.out.print("Qual categoria você pertence? [Individual ou Facultativo]\nResposta: ");
+		char categoriaEspecie = scan.next().charAt(0);
+		while (categoriaEspecie != 'i' && categoriaEspecie != 'I' && categoriaEspecie != 'f' && categoriaEspecie != 'F') {
+			System.out.print("Categoria inválida. Por favor, insire a categoria novamente: ");
+			categoriaEspecie = scan.next().charAt(0);
 		}
 		
-		
-
-		if (aliquota == 20) {
-			System.out.print("Sobre qual salário de contribuição deseja recolher?\nR: ");
-			double salarioInfo = scan.nextDouble();
-			valor = 20 * salarioInfo / 100;
-		} else if (aliquota == 11) {
-			valor = 11 * salarioMin / 100;
-		} else {
-			valor = 5 * salarioMin / 100;
-		}
-		System.out.printf("O valor da parcela a ser paga é R$%.2f", valor);
-
+		System.out.print("Está dando certo");
 		scan.close();
 
 	}
