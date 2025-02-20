b15d8d490b44f8bc54ad48c2de30a792264ceb76
Renaisa <renaisaalves@gmail.com>
2025-02-20 05:03:16 -0300

    atualizando

--- src/aulas/Funcoes.java
+++ src/aulas/Funcoes.java
@@ -35,10 +35,16 @@ public class Funcoes {
 		return aux;
 	}
 	
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
+	}
+	
 	//Toda função é criada abaixo da função principal "public static void main". Uma função não pode ser criada dentro da função principal "public static void main". 
 	//O objetivo da função max é processar o maior número entre os 3 digitados pelo usuário. Então a função max vai receber como argumento os 3 números e a partir daí a lógica é desenvolvida (igual como fazíamos dentro da função principal "public static void main"). 
 	//Ao final da instrução lógica desenvolvida, a palavra "return" (retorno) vai retornar a varável aux, que serviu para armazenar o maior valor.
 	//Toda função possui uma funcionalidade específica. Ela pode ser chamada dentro de um programa a qualquer momento (verde-limão), e pode ou não receber parâmetros/argumentos. Nesse exemplo, a função max pode receber 3 argumentos. 
 	//Repare que "a, b e c" são variáveis que foram criadas para armazenar valores numéricos do tipo int, mas também é possível colocar números diretamente na função. Isso significa que não necessariamente precisa ser só variáveis a servirem como parâmetros/argumentos, valores diretos também (exemplo: 3, 5 e 10) [desde que atendendo o tipo correto].
+	
+	//A segunda função tem objetivo de apenas imprimir na tela um argumento, por isso ela é do tipo (void). 
 
 }
