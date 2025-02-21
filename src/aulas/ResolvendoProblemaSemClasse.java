f080fbd15d381f3be30c3cd7196e135c970c8893
Renaisa <renaisaalves@gmail.com>
2025-02-21 18:56:18 -0300

    atualizando

---
+++ src/aulas/ResolvendoProblemaSemClasse.java
@@ -0,0 +1,18 @@
+package aulas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ResolvendoProblemaSemClasse {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		
+		scan.close();
+
+	}
+
+}
