70c0d668dfdef4d1270b03d2f7e763d80de718ba
Renaisa <renaisaalves@gmail.com>
2025-02-22 12:36:51 -0300

    atualizando exercício

--- src/exerciciosAvulsos/Calorias.java
+++ src/exerciciosAvulsos/Calorias.java
@@ -9,35 +9,24 @@ public class Calorias {
 
 		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
-
-		double totCalories = 0;
-		char resp;
 		
-		do {
-			System.out.print("Alimento: ");
-			String food = scan.nextLine().toUpperCase();
-			System.out.print("Gramas: ");
-			double mg = scan.nextDouble();
-			System.out.print("Calorias: ");
-			double calories = scan.nextDouble();
-			System.out.print("Quantidade: ");
-			int quant = scan.nextInt();
-			if (quant > 1) {
-				mg *= quant;
-				calories *= quant;
-			}
-			System.out.format("\nAlimento: %s%nPeso: %.0f gramas%nCalorias: %.0fkcal\n", food, mg, calories);
-			for (int i=0; i<quant; i++) {
-				totCalories = calories;
-			}
+		System.out.print("Alimento: ");
+		String alimento = scan.nextLine();
+		System.out.print("Gramas: ");
+		int gramas = scan.nextInt();
+		System.out.print("Calorias: ");
+		double calorias = scan.nextDouble();
+		System.out.print("Quantidade: ");
+		int quant = scan.nextInt();
+		
+		if (quant > 1) {
+			gramas *= quant;
+			calorias *= quant;
 			
-			System.out.print("Adicionar mais um alimento?\n[Sim/Não]: ");
-			resp = scan.next().charAt(0);
-		} while (resp == 'S' || resp == 's');
+		}
 		
-
-		System.out.printf("Total de calorias: %.0fkcal", totCalories);
-
+		System.out.printf("\nAlimento: %s%nGramas: %d%nCalorias: %.0fkcal%n", alimento, gramas, calorias);
+		
 		scan.close();
 	}
 
