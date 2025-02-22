0aada7aa5eb213202704119ced1e01d86cc14217
Renaisa <renaisaalves@gmail.com>
2025-02-22 08:00:58 -0300

    atualizando

--- src/exerciciosAvulsos/Calorias.java
+++ src/exerciciosAvulsos/Calorias.java
@@ -4,25 +4,31 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Calorias {
-	
+
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("CONTADOR DE CALORIAS");
-		
-		System.out.print("Nome do alimento: ");
-		String food = scan.next().toUpperCase();
-		System.out.print("Quantidade/porção/peso: ");
-		int quant = scan.nextInt();
-		System.out.print("Calorias: ");
-		double calories = scan.nextDouble();
-		
-		System.out.print("Deseja adicionar mais algum alimento? ");
-		char resp = scan.next().charAt(0);
-		System.out.print("Está tudo dando certo " + food + calories + quant);
-		
+
+		char resp;
+		do {
+			System.out.print("Nome do alimento: ");
+			String food = scan.next().toUpperCase();
+			System.out.print("Peso: ");
+			double peso = scan.nextDouble();
+			System.out.print("Calorias: ");
+			double calories = scan.nextDouble();
+			System.out.print("Quantidade: ");
+			int quant = scan.nextInt();
+
+			System.out.print("\nDeseja adicionar mais algum alimento?");
+			resp = scan.next().charAt(0);
+		} while (resp == 's' || resp == 'S');
+
+		System.out.printf("\nAlimento(s): %s%nQuantidade: %d%nCalorias: %.2fkcal%n", food, quant, calories);
+
 		scan.close();
 	}
 
