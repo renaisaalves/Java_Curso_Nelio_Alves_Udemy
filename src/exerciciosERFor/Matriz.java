deb12d813088403833028aa47029d9883bfc7f59
Renaisa <renaisaalves@gmail.com>
2025-02-26 04:29:56 -0300

    atualização

---
+++ src/exerciciosERFor/Matriz.java
@@ -0,0 +1,19 @@
+package exerciciosERFor;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 10;
+		
+		for (int i=0; i < x; i++) {
+			System.out.println(i + " " + x + " " );
+			for (int j=0; j < y; j++) {
+				System.out.println(j + " " + y + " " );
+			}
+		}
+
+	}
+
+}
