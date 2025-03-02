699e0b47190375a410ed38e69d4d8a1d62f6b940
Renaisa <renaisaalves@gmail.com>
2025-03-02 16:57:25 -0300

    atualização

---
+++ src/classes/Product.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class Product {
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double TotalValueInStock() {
+		return;
+	}
+	
+	public void AddProducts(int quantity) {
+		
+	}
+	
+	public void RemoveProducts(int quantity) {
+		
+	}
+
+}
