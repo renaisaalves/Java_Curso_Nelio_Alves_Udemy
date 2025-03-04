822ab32d23f270d40eb82cbec67cad25cd0dae2f
Renaisa <renaisaalves@gmail.com>
2025-03-04 11:25:21 -0300

    atualizando

--- src/classes/Retangulo.java
+++ src/classes/Retangulo.java
@@ -1,5 +1,29 @@
 package classes;
 
 public class Retangulo {
+	
+	public double width;
+	public double height;
+	
+	public double area() {
+		double a = width * height;
+		return a;
+	}
+	
+	public double perimeter() {
+		double p = 2 * (width * height);
+		return p;
+	}
+	
+	public double diagonal() {
+		
+		double d = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+		return d;
+		
+	}
+	
+	public String toString(String result) {
+		return "Altura: %.1f%n" + area() + "Largura: %1.f%n" + perimeter() + "Diagonal: %1.f%n" + diagonal();
+	}
 
 }
--- src/exerciciosClasses/Exercicio1_Retangulo.java
+++ src/exerciciosClasses/Exercicio1_Retangulo.java
@@ -1,9 +1,30 @@
 package exerciciosClasses;
 
+import java.util.Locale;
+import java.util.Scanner;
+import classes.Retangulo;
+
 public class Exercicio1_Retangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// 1) Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o valor de
+		// sua área, perímetro e diagonal. Usar uma classe como mostrado no projeto ao lado.
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		Retangulo calRetangulo = new Retangulo();
+		
+		System.out.print("Calculando medidas de um Retângulo\nLargura: ");
+		calRetangulo.width = scan.nextDouble();
+		
+		System.out.print("Altura: ");
+		calRetangulo.height = scan.nextDouble();
+		
+		System.out.printf("Área: %.2f%nPerímetro: %.2f%nDiagonal: %.2f%n", calRetangulo.area(), calRetangulo.perimeter(), calRetangulo.diagonal());
+		
+		scan.close();
 
 	}
 
